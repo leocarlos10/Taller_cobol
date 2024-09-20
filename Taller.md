5292644340c13bb29cce01e9c58ef111a47ff852
calebsenm <calebsenm@gmail.com>
2024-09-20 16:02:54 -0500

    add readme database.tx

---
+++ Taller.md
@@ -0,0 +1,9 @@
+#### Ejercicio en Clase
+
++ Crear un Archivo de Empleados que tenga Cedula, Nombre, Dirección, Teléfono y Salario-Básico
++ Ingresar 10 registros
++ Mostrar Todos los empleados 
++ Mostrar el Empleado Que mas Gana
++ Sumar El Total de La nómina
++ Calcular El Promedio de Sueldos Básicos de la Empresa.
+
--- Taller1.cbl
+++ Taller1.cbl
@@ -12,13 +12,14 @@
        FILE-CONTROL.
        SELECT OPTIONAL EMPLEADO-ARCHIVO
            ASSIGN TO
-           "C:\Users\USUARIO\OneDrive\Documentos\archivos\Taller1.txt"
+           "database.txt"
            ORGANIZATION IS SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
+
        FD EMPLEADO-ARCHIVO.
-           01 Empleados-registro.
+         01 Empleados-registro.
                05 Empleado-cedula PIC x(11).
                05 Empleado-nombre PIC x(30).
                05 Empleado-direccion PIC x(20).
@@ -26,18 +27,25 @@
                05 Empleado-salario-basico PIC 9(5)v999999.
 
        WORKING-STORAGE SECTION.
-           77 cedula PIC x(17) VALUE "Ingresa tu cedula".
-           77 nombre PIC x(17) VALUE "Ingresa tu nombre".
-           77 direccion PIC x(22) VALUE "Ingresa tu direccion".
-           77 telefono PIC x(20) VALUE "Ingresa tu telefono".
-           77 salario-basico PIC x(25)
-           VALUE "Ingresa tu salario basico".
-           77 si-no PIC x.
-           77 entrada PIC x.
+
+         77 cedula PIC x(17)    VALUE  "Ingresa tu cedula".
+         77 nombre PIC x(17)    VALUE  "Ingresa tu nombre".
+         77 direccion PIC x(22) VALUE  "Ingresa tu direccion".
+         77 telefono PIC x(20)  VALUE  "Ingresa tu telefono".
+         77 salario-basico PIC x(25) VALUE "Ingresa tu salario basico".
+         77 si-no PIC x.
+         77 entrada PIC x.
+         77 opcion PIC x.
+
+           
 
        PROCEDURE DIVISION.
+
        MAIN-LOGIC SECTION.
+
        Empezar-programa.
+           PERFORM INTERFAZ-APP. 
+
            PERFORM Abrir-archivo.
            MOVE "S" TO si-no.
            PERFORM Agregar-registro
@@ -45,11 +53,27 @@
            PERFORM cerrar-registro.
            STOP RUN.
 
+       Interfaz-App.
+           DISPLAY "-----------------------------------".
+           DISPLAY " Bienvenido Al Sistemas Empleados  ".
+           DISPLAY "-----------------------------------".
+
+           DISPLAY "1. Mostrar todos los empleados".
+           DISPLAY "2. Ingresar empleados".
+           DISPLAY "3. Mostrar empleado Que mas gana".
+           DISPLAY "4. Total de la nomina".
+           DISPLAY "5. Calcular Promedio Sueldos Basicos".
+
+           DISPLAY "Selecione > ".
+           ACCEPT  opcion.
+
+      
+       Abrir-archivo.
+           OPEN EXTEND EMPLEADO-ARCHIVO.
+       
        cerrar-registro.
            CLOSE EMPLEADO-ARCHIVO.
 
-       Abrir-archivo.
-           OPEN EXTEND EMPLEADO-ARCHIVO.
 
        Agregar-registro.
            MOVE "N" TO entrada.
@@ -61,7 +85,6 @@
 
        Escribir-registro.
            WRITE Empleados-registro.
-
 
        Obtener-campos.
            MOVE SPACE TO Empleados-registro.
@@ -77,18 +100,20 @@
            ACCEPT Empleado-salario-basico.
            PERFORM Continuar.
 
-
        Continuar.
            MOVE "S" TO entrada.
+
            if Empleado-nombre = SPACE
-           MOVE "N" TO entrada.
+               MOVE "N" TO entrada.
 
        Reiniciar.
-           DISPLAY "¿Desea ingresar otro registro (S/N) ?".
-          ACCEPT SI-NO.
-          IF SI-NO = "s"
-          MOVE "S" TO SI-NO.
-          IF SI-NO NOT = "S"
-          MOVE "N" TO SI-NO.
+           DISPLAY "Desea ingresar otro registro (S/N) ?".
+           ACCEPT SI-NO.
+
+           IF SI-NO = "s"
+               MOVE "S" TO SI-NO.
+
+           IF SI-NO NOT = "S"
+               MOVE "N" TO SI-NO.
 
        END PROGRAM Taller1.
