7ff0ffeb2faef9a33fb96ab47e206f98d77cfbf7
leocarlos10 <leocarlosospinacausil10@gmail.com>
2024-09-20 18:13:48 -0500

    subiendo cambios de leer datos

--- Taller1.cbl
+++ Taller1.cbl
@@ -27,7 +27,22 @@
                05 Empleado-salario-basico PIC 9(8).
 
        WORKING-STORAGE SECTION.
-
+      * variables para poder mostrar los empleados.
+       01 Presentacion.
+           05 Texto-cedula PIC x(5) VALUE "ID: ".
+           05 Muestra-cedula PIC x(17).
+           05 Texto-nombre PIC x(10) VALUE "Nombre: ".
+           05 Muestra-nombre PIC x(10).
+           05 Texto-direccion PIC x(11) VALUE "direccion: ".
+           05 Muestra-direccion PIC x(10).
+           05 Texto-telefono PIC x(10) VALUE "telefono: ".
+           05 Muestra-telefono PIC x(10).
+           05 Texto-salario-basico PIC x(19) VALUE "salario basico: ".
+           05 Muestra-salario-basico PIC x(15).
+           01  Fin-Del-Archivo PIC X.
+           01  Maximos-Registros PIC 99.
+           01  Guarda-Enter PIC X.
+      * variables para guardar los datos de los empleados.
          77 cedula PIC x(17)    VALUE  "Ingresa tu cedula".
          77 nombre PIC x(17)    VALUE  "Ingresa tu nombre".
          77 direccion PIC x(22) VALUE  "Ingresa tu direccion".
@@ -47,7 +62,14 @@
            PERFORM INTERFAZ-APP.
 
            IF OPCION = 1
-               DISPLAY "Mostrando Empleados".
+               PERFORM  Apertura-archivo.
+               MOVE ZEROES TO Maximos-Registros.
+               MOVE "1" TO Fin-Del-Archivo.
+               PERFORM Lee-Siguiente-Registro.
+               PERFORM  Muestra-Registro
+               UNTIL Fin-Del-Archivo = "0".
+               PERFORM cerrar-registro.
+               STOP RUN.
            IF OPCION = 2
                PERFORM Abrir-archivo
                MOVE "S" TO si-no
@@ -56,7 +78,8 @@
                PERFORM cerrar-registro.
 
            IF OPCION = 3
-              DISPLAY "Mostrar Empleado que mas gana".
+               PERFORM Apertura-archivo.
+
 
            IF OPCION = 4
               DISPLAY "Total de La nomina".
@@ -84,7 +107,39 @@
            DISPLAY "Selecione > ".
            ACCEPT  opcion.
 
+      * codigo para mostrar todos los empleados
+       Apertura-archivo.
+       OPEN INPUT EMPLEADO-ARCHIVO.
 
+        Muestra-Registro.
+           PERFORM Muestra-campos.
+           PERFORM Lee-Siguiente-Registro.
+
+
+        Muestra-campos.
+           IF Maximos-Registros = 10
+           PERFORM PULSAR-ENTER.
+           MOVE Empleado-cedula TO Muestra-cedula.
+           MOVE Empleado-nombre TO Muestra-nombre.
+           MOVE Empleado-direccion TO Muestra-direccion.
+           MOVE Empleado-telefono TO Muestra-telefono.
+           MOVE Empleado-salario-basico TO Muestra-salario-basico.
+           DISPLAY PRESENTACION.
+           ADD 1 TO Maximos-Registros.
+
+
+       Lee-Siguiente-Registro.
+           READ EMPLEADO-ARCHIVO NEXT RECORD
+           AT END
+           MOVE "0" TO FIN-DEL-ARCHIVO.
+
+       PULSAR-ENTER.
+           DISPLAY
+           "Presione la tecla ENTER para ver la siguiente pagina...".
+           ACCEPT Guarda-Enter.
+           MOVE ZEROES TO Maximos-Registros.
+
+      * codigo para registrar Empleados
        Abrir-archivo.
            OPEN EXTEND EMPLEADO-ARCHIVO.
 
@@ -132,5 +187,6 @@
 
            IF SI-NO NOT = "S"
                MOVE "N" TO SI-NO.
+
 
        END PROGRAM Taller1.
